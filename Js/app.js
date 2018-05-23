9bd1e2ee2c97a8471b5abe9d770c5d6a80952c46
srikanthreddy376 <srikanthreddy376@gmail.com>
2018-05-24 00:34:42 +0200

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -1,16 +1,20 @@
 
-// Animations using j-query on index page
+//------ Animations using j-query on index page-----
 
 $('#welcome').addClass('animated bounceInLeft');
 $('#welcome').one('webkitAnimationEnd', function(){
   $('#welcome').removeClass('animated bounceInLeft');
 });
+ //----Adding animation effect using the for the header-----
 
 $('.intro').addClass('animated bounceInRight')
 $('.intro').one('webkitAnimationEnd', function(){
-  $('.intro').removeClass('animated bounceInRight');
+ $('.intro').removeClass('animated bounceInRight');
 });
 $('button').addClass('animated fadeIn');
+
+
+//-----button animations---
 
 $('button').mouseover(function(){
   $('button').removeClass('animated fadeIn');
