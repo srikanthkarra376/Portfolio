00f5e6a4ad5305b268dc392375eb3171a55bb63b
srikanth Karra <srikanthreddy376@gmail.com>
2018-06-03 16:17:25 +0200

    remove fixed nav and added new styling to nav bar and to all pages

--- Js/app.js
+++ Js/app.js
@@ -1,4 +1,12 @@
-let links = document.querySelectorAll('#links .row li a');
+/* Shrink nav 
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 100) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
+ */
 
 /* links.forEach(function(link){
  link.addEventListener('click',function(lnk){
@@ -12,13 +20,34 @@ let links = document.querySelectorAll('#links .row li a');
 }); */
 
 
-links.forEach(function(link){
-link.addEventListener('click',function(){
-   if(link.classList=='active'){
-       link.classList.remove('active')
-   }else {
-     link.classList.add('active')
-   }
-});
 
+
+
+let link= document.querySelector('#links .row li a');
+
+link.addEventListener('click',function(e){
+  e.preventDefault();
+  if(link.classList == 'js'){
+    link.classList.remove('js')
+  }else{
+    link.classList.add('js');
+  }
+
+})
+//enabling smooth scrolling==
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
