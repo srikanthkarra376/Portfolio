a86e03b9f67ed5b660d916eac09e37b3ac93680d
srikanth Karra <srikanthreddy376@gmail.com>
2018-06-03 12:34:01 +0200

    Added JS code to handle event listeners

--- Js/app.js
+++ Js/app.js
@@ -1,2 +1,24 @@
+let links = document.querySelectorAll('#links .row li a');
 
- 
+/* links.forEach(function(link){
+ link.addEventListener('click',function(lnk){
+  if(lnk.classList.contains('active')){
+    lnk.classList.remove('active');
+  }else{
+    lnk.classList.add('active');
+  }
+ });
+
+}); */
+
+
+links.forEach(function(link){
+link.addEventListener('click',function(){
+   if(link.classList=='active'){
+       link.classList.remove('active')
+   }else {
+     link.classList.add('active')
+   }
+});
+
+});
