a843179df501eed5d06102a148ffd43b20dbad43
srikanth Karra <srikanthreddy376@gmail.com>
2018-07-04 13:01:52 +0200

    New portfolio

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+      || location.hostname == this.hostname) {
+
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1500);
-        return false;
+         if (target.length) {
+           $('html,body').animate({
+               scrollTop: target.offset().top
+          }, 1000);
+          return false;
       }
-    }
-  });
+  }
 });
+
+// var Icon = document.querySelector("div#icon");
+// var uldiv = document.querySelector(".menu");
+// Icon.addEventListener('click',function(e){
+//    uldiv.
+// }
+
+$("div#icon").on("click",function(){
+    $('.menu').toggle();
+})
