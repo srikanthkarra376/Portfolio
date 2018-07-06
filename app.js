246d9be8f7468b1ffdbec45ed75d603aa79ba26f
srikanth Karra <srikanthreddy376@gmail.com>
2018-07-06 12:17:08 +0200

    new styling for media screen

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-
-
-$("#first #logo").on("click",function(){
-    $('#menu-ul').toggle();
-});
-
 $(window).scroll(function() {
   if ($(this).scrollTop() >= 100) {       
       $('#top').fadeIn(200);    
@@ -16,47 +10,50 @@ $('#top').click(function() {
       scrollTop : 0                       
   }, 1000);
 });
+
+
+
+//smooth scrolling 
  
     
+$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
 
-
-
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
       && 
       location.hostname == this.hostname
     ) {
-      // Figure out element to scroll to
+     
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
+     
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
+       
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
         }, 1500, function() {
-          // Callback after animation
-          // Must change focus!
+       
           var $target = $(target);
           $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
+          if ($target.is(":focus")) { 
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
+            $target.attr('tabindex','-1');
+            $target.focus(); 
           };
         });
       }
     }
   });
+
+  //side nav bar finctions
+  $('#first #logo i').on('click',function(){
+    $("#menu #menu-ul").css('width','150').show().fadeIn(1000);
+   });
+   $('#menu #menu-ul li a.closebtn').click(function(){
+     $("#menu #menu-ul").hide();
+   });
 
 
   $(window).scroll(function() {
@@ -66,6 +63,7 @@ $('a[href*="#"]')
         $('img').fadeOut(1000);   
     }
   });
+
 
   
   
